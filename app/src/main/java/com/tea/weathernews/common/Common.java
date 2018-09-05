f32e2a83055c2b42e0bdeada0108603ed844a94d
Nguyen Duc Thien <thien.nguyenduc@nccsoft.vn>
2018-09-05 10:11:06 +0700

    Weather app with retrofit hihi

---
+++ app/src/main/java/com/tea/weathernews/common/Common.java
@@ -0,0 +1,10 @@
+package com.tea.weathernews.common;
+
+import android.location.Location;
+
+public class Common {
+
+    public static final String APP_API = "c45f73e836a719f5c3fd9dd292206ab1";
+    public static Location current_location = null;
+
+}
---
+++ app/src/main/java/com/tea/weathernews/retrofit/IOpenWeatherMap.java
@@ -0,0 +1,16 @@
+package com.tea.weathernews.retrofit;
+
+import com.tea.weathernews.model.WeatherResult;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface IOpenWeatherMap {
+    @GET("weather")
+    Observable<WeatherResult> getWeatherByLatLong(@Query("lat") String lat,
+                                                  @Query("long") String lng,
+                                                  @Query("appid") String appid,
+                                                  @Query("units") String units);
+
+}
